e0609941d1c0f45209404e3925d18cb1895086ed
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2024-06-06 18:30:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let numero = 0
+
+function somar(){
+    numero = numero + 1
+    document.getElementById("bt").innerText = numero
+}
+
+let nome = "Janaina "
+let sobrenome = "Tavares"
+
+document.getElementById("nome").innerText = "Esse texto foi gerado com uma variável: " + nome + sobrenome
+
